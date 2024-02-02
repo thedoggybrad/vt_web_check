dadebbb09ee6d3c99a51605bcdf5456031f7586f
TheDoggyBrad Software Lab <94173621+thedoggybrad@users.noreply.github.com>
2024-02-02 17:59:31 +0800

    Add files via upload

---
+++ Chromium (Manifest v3)/background.js
@@ -0,0 +1,22 @@
+chrome.action.onClicked.addListener(function (tab) {
+  const input = tab.url;
+  const formattedInput = doubleEncodeURL(input);
+  const link = formattedInput ? `https://www.virustotal.com/gui/search/${formattedInput}` : null;
+
+  if (link) {
+    openInNewTab(link);
+  }
+});
+
+const openInNewTab = (url) => {
+  chrome.tabs.create({ url: url });
+};
+
+const doubleEncodeURL = (input) => {
+  try {
+    const url = new URL(input);
+    return encodeURIComponent(encodeURIComponent(url.href));
+  } catch (error) {
+    return null;
+  }
+};
